232be55cfdb6f01f9e624788b8eaad6e21de7279
Jedeu <jed.piezas@gmail.com>
2016-03-07 19:53:38 +0000

    Basic gaming logic

--- bettinggame.js
+++ bettinggame.js
@@ -1,7 +1,27 @@
-var bet = prompt('Place a bet between $5 and $10. No cents please!');
+"use strict";
 
-var guess = prompt('Guess a number between 1 and 10. No decimals please!')
+var bankroll = 100;
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+while (bankroll) > 0 {
+  var bet = prompt('Place a bet between $5 and $10. No cents please!');
+
+  var guess = prompt('Guess a number between 1 and 10. No decimals please!');
+
+  var RNGGod = function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  var ranNum = RNGGod(1, 10);
+
+  if (guess == ranNum) {
+    alert('Your guess was right!');
+    bankroll += bet;
+  }
+  else if ((guess - ranNum) == Math.abs(1) || (guess + ranNum) == Math.abs(1)) {
+    alert('Close! You do not gain nor lose money');
+  }
+  else {
+    alert('Nope, you lose $' + bet);
+    bankroll -= bet;
+  }
 }
