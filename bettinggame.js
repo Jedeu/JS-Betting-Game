46d6b44d5f16525ef4bcf0857726a45788e738a3
Jedeu <jed.piezas@gmail.com>
2016-03-07 19:23:03 +0000

    Added RNG function

--- bettinggame.js
+++ bettinggame.js
@@ -1,3 +1,7 @@
 var bet = prompt('Place a bet between $5 and $10. No cents please!');
 
 var guess = prompt('Guess a number between 1 and 10. No decimals please!')
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
